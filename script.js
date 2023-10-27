cb4d6f819de62f1a1e1fd9b2ed5af4beb43d7ff7
Mehboob Elahi <mehboob4ias@gmail.com>
2023-10-27 14:50:41 +0530

    commit

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function pageOpen() {
 }
 
 function onSignIn(googleUser) {
+    debugger;
     var profile = googleUser.getBasicProfile();
     console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log("Name: " + profile.getName());
